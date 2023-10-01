d89afd9c2f0c1fdf97641b564025992789af1df3
tempx9 <elviskaraev14@gmail.com>
2023-10-01 11:29:48 +0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() 
-{
-  int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  int x = A[2]; // Ссылка может указывать на элемент массива
-  cout << x;
-}
