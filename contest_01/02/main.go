61dd3097041bd82df403f5f5abd593fdc36c793b
tempx9 <elviskaraev14@gmail.com>
2023-10-01 10:39:00 +0300

    Update main.go

--- contest_01/02/main.go
+++ contest_01/02/main.go
@@ -1,1 +1,18 @@
+package main
 
+import (
+  "fmt"
+  "math"
+)
+
+func main() {
+  var human float64 = 0.5
+  var oak float64 = 20
+  var poplar float64 = 32
+
+  human = 365 * human
+  oak1 := math.Ceil(human / oak)
+  poplar1 := math.Ceil(human / poplar)
+  
+  fmt.Println(human, poplar1, oak1)
+}
