09e84a788d6a158aca5c24ea4aea43206d375d6d
tempx9 <elviskaraev14@gmail.com>
2023-10-01 10:36:39 +0300

    Update main.cpp

--- contest_01/01/main.cpp
+++ contest_01/01/main.cpp
@@ -1,1 +1,7 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+  cout << sqrt(12) * (1 - 1.0 / (3 * 3) + 1.0 / (5 * pow(3, 2)) - 1.0 / (7 * pow(3, 3)) + 1.0 / (9 * pow(3, 4)) - 1.0 / (11 * pow(3, 5)));
+}
